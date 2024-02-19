3f7a8950a849aa86e5f98d4b8d71e594fd790ff8
Jacob Lauxman <jlauxman@gmail.com>
2024-02-19 16:52:51 -0700

    First integration test for `health_check` route

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,6 @@
-use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
-
-// async fn greet(req: HttpRequest) -> impl Responder {
-//     let name = req.match_info().get("name").unwrap_or("werld");
-//     format!("Hello {} !", &name)
-// }
-
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok().finish()
-}
-
+use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    HttpServer::new(|| {
-        App::new()
-        // .route("/", web::get().to(greet))
-        // .route("/{name}", web::get().to(greet))
-        .route("/health_check", web::get().to(health_check))
-    })
-    .bind("127.0.0.1:8080")?
-    .run()
-    .await
+    run()?.await
 }

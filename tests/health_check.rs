c3a55693e296e9f9ac651e50da8622b0af94d507
Jacob Lauxman <jlauxman@gmail.com>
2024-02-19 17:08:02 -0700

    Refactored logic of `run` functionality for more robust testing capabilities re: `health_check` route

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,12 +1,14 @@
+use std::net::TcpListener;
+
 #[tokio::test]
 async fn health_check_works() {
     // Arrange
-    spawn_app();
+    let addr = spawn_app();
     let client = reqwest::Client::new();
 
     // Act
     let res = client
-        .get("http://127.0.0.1:8000/health_check")
+        .get(&format!("{}/health_check", &addr))
         .send()
         .await
         .expect("Failed to execute request");
@@ -16,7 +18,13 @@ async fn health_check_works() {
     assert_eq!(Some(0), res.content_length());
 }
 
-fn spawn_app() {
-    let server = zero2prod::run().expect("Failed to bind address");
+// this helper creates app process and additionally returns our needed port-bound app address
+fn spawn_app() -> String {
+    // at OS level - trying to bind port 0 will have OS scan for available port to then bind our app instance!
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to random port");
+    let port = listener.local_addr().unwrap().port();
+    let server = zero2prod::run(listener).expect("Failed to bind address");
     let _ = tokio::spawn(server);
+
+    format!("http://127.0.0.1:{}", port)
 }

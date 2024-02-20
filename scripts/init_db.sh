625b5cf81a279f18b6fa5733b782134ed658bd13
Jacob Lauxman <jlauxman@gmail.com>
2024-02-19 18:18:06 -0700

    database initialization and connection setup with script command

---
+++ scripts/init_db.sh
@@ -0,0 +1,71 @@
+#!/usr/bin/env bash
+
+# NOTE: to make this file executable run
+# chmod +x scripts/init_db.sh
+# then -> ./scripts/init_db.sh
+# --> can skip docker postgres setup (if already initialized) via flag SKIP_DOCKER=true
+
+set -x
+set -eo pipefail
+
+
+# Tests dependencies (psql, sqlx) are installed before running the rest of script:
+if ! [ -x "$(command -v psql)" ]; then
+  echo >&2 "Error: psql is not installed."
+  exit 1
+fi
+
+if ! [ -x "$(command -v sqlx)" ]; then
+  echo >&2 "Error: sqlx is not installed."
+  echo >&2 "Use:"
+  echo >&2 "    cargo install --version='~0.7' sqlx-cli \
+--no-default-features --features rustls,postgres"
+  echo >&2 "to install it."
+  exit 1
+fi
+
+
+# Setting DB connection string variables:
+# Check if a custom user has been set, otherwise default to 'postgres'
+DB_USER="${POSTGRES_USER:=postgres}"
+# Check if a custom password has been set, otherwise default to 'password'
+DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
+# Check if a custom database name has been set, otherwise default to 'newsletter'
+DB_NAME="${POSTGRES_DB:=newsletter}"
+# Check if a custom port has been set, otherwise default to '5432'
+DB_PORT="${POSTGRES_PORT:=5432}"
+# Check if a custom host has been set, otherwise default to 'localhost'
+DB_HOST="${POSTGRES_HOST:=localhost}"
+
+
+# Checks whether dockerized postgres DB already running (skips if so)
+if [[ -z "${SKIP_DOCKER}" ]]
+then
+# Launch postgres using Docker
+  docker run \
+    -e POSTGRES_USER=${DB_USER} \
+    -e POSTGRES_PASSWORD=${DB_PASSWORD} \
+    -e POSTGRES_DB=${DB_NAME} \
+    -p "${DB_PORT}":5432 \
+    -d postgres \
+    postgres -N 1000
+    # ^ Increased maximum number of connections for testing purposes
+fi
+
+# Keep pinging Postgres until it's ready to accept commands
+export PGPASSWORD="${DB_PASSWORD}"
+until psql -h "${DB_HOST}" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
+  >&2 echo "Postgres is still unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "Postgres is up and running on port ${DB_PORT}!"
+
+
+# Connects sqlx to postgres instance and runs migrations
+DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}
+export DATABASE_URL
+sqlx database create
+sqlx migrate run
+
+>&2 echo "Postgres db migrated, ready to go!"
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,5 +1,7 @@
 use std::net::TcpListener;
 
+// HEALTH CHECK testing
+
 #[tokio::test]
 async fn health_check_works() {
     // Arrange
@@ -27,4 +29,58 @@ fn spawn_app() -> String {
     let _ = tokio::spawn(server);
 
     format!("http://127.0.0.1:{}", port)
+}
+
+// SUBSCRIBE testing
+
+#[tokio::test]
+async fn subscribe_returns_200_for_valid_form_data() {
+    // Arrange
+    let app_addr = spawn_app();
+    let client = reqwest::Client::new();
+
+    // Act
+    let body = "name=mj%20hohams&email=mjhohams%40gmail.com";
+    let res = client
+        .post(&format!("{}/subscriptions", &app_addr))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute POST request");
+
+    // Assert
+    assert_eq!(200, res.status().as_u16());
+}
+
+#[tokio::test]
+async fn subscribe_returns_400_when_form_data_missing() {
+    // Arrange
+    let app_addr = spawn_app();
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=mj%20hohams", "missing the email"),
+        ("email=mj_hohams%40gmail.com", "missing the name"),
+        ("", "missing both name and email"),
+    ];
+
+    for (inv_body, err_msg) in test_cases {
+        // Act
+        let res = client
+            .post(&format!("{}/subscriptions", &app_addr))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(inv_body)
+            .send()
+            .await
+            .expect("Failed to execute POST request");
+
+        // Assert
+        assert_eq!(
+            400,
+            res.status().as_u16(),
+            // allows additional info re: err_msg on test failure
+            "The API did not fail with 400 Bad Request when payload should have been {}",
+            err_msg
+        );
+    }
 }

af4b4bf17d6f69041724d025255bc95dd9967580
Jacob Lauxman <jlauxman@gmail.com>
2024-03-06 15:57:15 -0700

    Refactoring `/login` route to include redirects, parms re: error msgs on auth failure and XSS protection of params via `htmlescape` crate

--- src/routes/login/get.rs
+++ src/routes/login/get.rs
@@ -1,8 +1,48 @@
-use actix_web::http::header::ContentType;
-use actix_web::HttpResponse;
+use actix_web::{http::header::ContentType, web, HttpResponse};
 
-pub async fn login_form() -> HttpResponse {
+#[derive(serde::Deserialize)]
+pub struct QueryParams {
+    // we've encoded any potential err msg from LoginError to pass via query params in response redirect url!
+    error: Option<String>,
+}
+
+pub async fn login_form(query: web::Query<QueryParams>) -> HttpResponse {
+    let err_html = match query.0.error {
+        None => "".into(),
+        Some(err_msg) => format!("<p><i>{}</i></p>", htmlescape::encode_minimal(&err_msg)),
+        // `htmlescape` escapes html chars w/ entity-encoding (for XSS prevention)
+    };
+
     HttpResponse::Ok()
         .content_type(ContentType::html())
-        .body(include_str!("login.html"))
+        // .body(include_str!("login.html"))
+        .body(format!(
+            r#"<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8">
+        <title>Login</title>
+    </head>
+    <body>
+        {err_html}
+        <form action="/login" method="post">
+            <label>Username
+                <input
+                type="text"
+                placeholder="Enter Username"
+                name="username"
+                />
+            </label>
+            <label>Password
+                <input
+                type="password"
+                placeholder="Enter Password"
+                name="password"
+                />
+            </label>
+            <button type="submit">Login</button>
+        </form>
+    </body>
+    </html>"#,
+        ))
 }
